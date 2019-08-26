a4f9225dd3178ef5b223ac210607bae98cf688d6
William Blake Galbreath <blake.galbreath@gmail.com>
2019-08-25 21:14:57 -0500

    Add broadcast command

--- src/main/java/net/pl3x/bukkit/discord4bukkit/command/bukkit/CmdDiscord4Bukkit.java
+++ src/main/java/net/pl3x/bukkit/discord4bukkit/command/bukkit/CmdDiscord4Bukkit.java
@@ -3,10 +3,11 @@ package net.pl3x.bukkit.discord4bukkit.command.bukkit;
 import net.pl3x.bukkit.discord4bukkit.D4BPlugin;
 import net.pl3x.bukkit.discord4bukkit.configuration.Config;
 import net.pl3x.bukkit.discord4bukkit.configuration.Lang;
-import net.pl3x.bukkit.discord4bukkit.task.ConsoleMessageQueueWorker;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
 import org.bukkit.command.TabExecutor;
+
+import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
@@ -16,7 +17,7 @@ public class CmdDiscord4Bukkit implements TabExecutor {
     public List<String> onTabComplete(CommandSender sender, Command command, String label, String[] args) {
         if (sender.hasPermission("command.discord4bukkit")) {
             if (args.length == 1) {
-                return Stream.of("reload", "start", "stop")
+                return Stream.of("reload", "start", "stop", "broadcast")
                         .filter(arg -> arg.startsWith(args[0].toLowerCase()))
                         .collect(Collectors.toList());
             }
@@ -34,26 +35,40 @@ public class CmdDiscord4Bukkit implements TabExecutor {
         D4BPlugin plugin = D4BPlugin.getInstance();
         String response = "&d" + plugin.getName() + " v" + plugin.getDescription().getVersion();
 
-        if (args.length > 0) {
-            if (args[0].equalsIgnoreCase("reload")) {
-                Config.reload();
-                Lang.reload();
-
-                response += " reloaded.";
-                Lang.send(sender, response);
-                return true;
-            }
-
-            if (args[0].equalsIgnoreCase("start")) {
-                plugin.getBot().connect();
-            }
-
-            if (args[0].equalsIgnoreCase("stop")) {
-                plugin.getBot().disconnect();
-            }
+        if (args.length == 0) {
+            Lang.send(sender, response);
+            return true;
         }
 
-        Lang.send(sender, response);
-        return true;
+        if (args[0].equalsIgnoreCase("reload")) {
+            Config.reload();
+            Lang.reload();
+
+            response += " reloaded.";
+            Lang.send(sender, response);
+            return true;
+        }
+
+        if (args[0].equalsIgnoreCase("start")) {
+            plugin.getBot().connect();
+            Lang.send(sender, "Bot started");
+            return true;
+        }
+
+        if (args[0].equalsIgnoreCase("stop")) {
+            plugin.getBot().disconnect();
+            Lang.send(sender, "Bot stopped");
+            return true;
+        }
+
+        if (args[0].equalsIgnoreCase("broadcast")) {
+            if (args.length < 2) {
+                return false;
+            }
+            plugin.getBot().sendMessageToDiscord(String.join(" ", Arrays.copyOfRange(args, 1, args.length)));
+            return true;
+        }
+
+        return false;
     }
 }
